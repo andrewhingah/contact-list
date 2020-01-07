5c77266b5d6ac64163740b1c9c8f2d3756fc44ad
Andrew Hinga <andrewhinga5@gmail.com>
2020-01-07 11:52:10 +0300

    create contact list page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Andrew Hinga</h5>
+        <h6 className="card-subtitle mb-2 text-muted">andrew@gmail.com</h6>
+        <p className="card-text">Stay Hungry, Stay Foolish</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;

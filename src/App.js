d3f4ddb5298069b3527be61f577cdee0350c5e43
Andrew Hinga <andrewhinga5@gmail.com>
2020-01-08 09:15:20 +0300

    create contacts component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">Andrew Hinga</h5>
-        <h6 className="card-subtitle mb-2 text-muted">andrew@gmail.com</h6>
-        <p className="card-text">Stay Hungry, Stay Foolish</p>
+class App extends Component {
+  state = {
+    contacts: []
+  };
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ contacts: data }).catch(err => console.log(err));
+      });
+  }
+  render() {
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Andrew Hinga</h5>
+          <h6 className="card-subtitle mb-2 text-muted">andrew@gmail.com</h6>
+          <p className="card-text">Stay Hungry, Stay Foolish</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

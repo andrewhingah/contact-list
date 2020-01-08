37fe6eb5d9af6e14232180a2a6d25f1897de22b2
Andrew Hinga <andrewhinga5@gmail.com>
2020-01-08 09:29:03 +0300

    render the contacts component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Contacts from "./components/contacts";
 import "./App.css";
 
 class App extends Component {
@@ -9,19 +10,12 @@ class App extends Component {
     fetch("http://jsonplaceholder.typicode.com/users")
       .then(res => res.json())
       .then(data => {
-        this.setState({ contacts: data }).catch(err => console.log(err));
-      });
+        this.setState({ contacts: data });
+      })
+      .catch(console.log);
   }
   render() {
-    return (
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">Andrew Hinga</h5>
-          <h6 className="card-subtitle mb-2 text-muted">andrew@gmail.com</h6>
-          <p className="card-text">Stay Hungry, Stay Foolish</p>
-        </div>
-      </div>
-    );
+    return <Contacts contacts={this.state.contacts} />;
   }
 }
 
